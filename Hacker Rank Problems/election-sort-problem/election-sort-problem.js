ebe022a414f2603fd3912c13029dc4c323c286e8
Benjamin Dirgo <benjamin.dirgo@gm.com>
2018-02-26 15:33:24 -0700

    Add Hacker Rank problems

---
+++ Hacker Rank Problems/election-sort-problem/election-sort-problem.js
@@ -0,0 +1,29 @@
+function electionWinner(votes) {
+    var countOfVotes = votes[0];
+    votes = votes.slice(1);
+    // First Sort
+    votes.sort();
+    // Second count the votes for each person
+    var countedNames = votes.reduce(function (allNames, name) { 
+        if (name in allNames) {
+            allNames[name]++;
+        }
+        else {
+            allNames[name] = 1;
+        }
+        return allNames;
+    }, {});
+    // Find the Max
+    var winningVotes = Object.values(countedNames).reduce( ( max, cur ) => Math.max( max, cur ), 0 );
+    var winnersCircle = [];
+    // Find all electors that have the max votes
+    for (var winner in countedNames) {
+        if (countedNames[winner] == winningVotes) {
+            winnersCircle.push(winner);
+        }
+    }
+    // Return the last person in that array
+    return winnersCircle[winnersCircle.length - 1];
+}
+    
+    console.log(electionWinner([10,"Alex","Micheal","Harry","Dave","Micheal","Victor","Harry","Alex","Mary","Mary"]))
